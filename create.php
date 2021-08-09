4eef8daccf33e2efb98d47132762de4751e3d13a
Nomawethu Jaceni <jacenin@yahoo.com>
2021-08-10 00:26:23 +0200

    commit message

---
+++ create.php
@@ -0,0 +1,30 @@
+<<?php 
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Form</title>
+ </head>
+ <body>
+ 	<center><h1>Add User</h1></center>
+ 	Name : <br>
+ 	<input type="text" name="Name">
+ 	<br>
+
+ 	Surname : <br>
+ 	<input type="text" name="Surname">
+ 	<br>
+ 	Email : <br>
+ 	<input type="email" name="Email">
+ 	<br>
+ 	Password : <br>
+ 	<input type="password" name="Password">
+ 	<br>
+ 	Gender : <br>
+ 	<input type="radio" name="gender" value="Male">
+ 	<input type="radio" name="gender" value="Female">
+ 	<br>
+ 	<input type="submit" name="submit" value="submit">
+ </body>
+ </html>
